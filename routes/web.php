c6784b514ece9d8d35b36d41b2f0a06226473871
Khant Min Kyi <khantminkyi@gmail.com>
2025-01-04 22:19:52 +0630

    add nav items to layout and change dashboard web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::redirect('/', '/dashboard');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', fn() => Inertia::render('Dashboard'))->name('dashboard');
     Route::resource('/projects', ProjectController::class);
-    Route::resource('/taks', TaskController::class);
+    Route::resource('/tasks', TaskController::class);
     Route::resource('/users', UserController::class);
 });
 
